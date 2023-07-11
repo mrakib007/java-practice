4699c505b159031e6bf1cd7a41ea2256e2eda37f
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-07-11 13:19:12 +0600

    Inverted half pyramid with numbers

--- patterns/HalfPyramidNumber.java
+++ patterns/HalfPyramidNumber.java
@@ -5,7 +5,7 @@ public class HalfPyramidNumber {
         int n = 5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
