ab10095e5a0ca42941a7f227e7e97fb87d9e11fb
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-07-16 10:54:52 +0600

    Selection sort

--- Sorting/SelectionSorting.java
+++ Sorting/SelectionSorting.java
@@ -1,6 +1,6 @@
 package Sorting;
 
-public class BubbleSorting {
+public class SelectionSorting {
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -11,17 +11,18 @@ public class BubbleSorting {
     public static void main(String[] args) {
         int arr[] = { 7, 8, 3, 1, 2 };
 
-        // bubble sort
+        // selection sort
         // time complexity = O(n^2)
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
                 }
             }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
         }
         printArray(arr);
     }
