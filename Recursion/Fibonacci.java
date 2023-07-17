190169c826e096e6a7106de0cebeb3c428bb990c
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-07-17 13:20:40 +0600

    Fibonacci series

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Fibonacci {
+    public static void printFibonacci(int a, int b, int n) {
+        if (n == 0) {
+            return;
+        }
+        int c = a + b;
+        System.out.println(c);
+        printFibonacci(b, c, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 10;
+        printFibonacci(a, b, n - 2);
+    }
+}
