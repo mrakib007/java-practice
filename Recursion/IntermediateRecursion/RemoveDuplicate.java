d5c3e3bf7456e1ee5e288b639924f39eed182510
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-07-21 10:24:53 +0600

    Removing repeated characters with recursion

---
+++ Recursion/IntermediateRecursion/RemoveDuplicate.java
@@ -0,0 +1,25 @@
+package Recursion.IntermediateRecursion;
+
+public class RemoveDuplicate {
+    public static boolean[] map = new boolean[26];
+
+    public static void removeDuplicate(String str, int index, String newString) {
+        if (index == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(index);
+        if (map[currChar - 'a'] == true) {
+            removeDuplicate(str, index + 1, newString);
+        } else {
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDuplicate(str, index + 1, newString);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abbcxxaayyz";
+        removeDuplicate(str, 0, "");
+    }
+}
